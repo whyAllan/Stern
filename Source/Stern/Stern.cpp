e0b12dcc2d99ad811aa39eccd6167ca6ee02cbaa
Allan Freitas <allanfreitas7.11@gmail.com>
2026-07-15 14:59:22 -0700

    Add project files.

---
+++ Source/Stern/Stern.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Stern.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Stern, "Stern" );
